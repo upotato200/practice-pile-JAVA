9ccb2f39b35839acfdeb0913735d5d25fd5fa60b
whlsls3377 <whlsls3377@naver.com>
2024-09-11 11:50:13 +0900

    commit update

---
+++ src/chapter05/SpartaProblem16.java
@@ -0,0 +1,21 @@
+package chapter05;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SpartaProblem16 {
+    public static List<Long> solution(int x, int n) {
+        List<Long> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            result.add((long) x * i); // 각 단계에서 x의 배수를 추가
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 테스트 예시
+        System.out.println(solution(2, 5)); // [2, 4, 6, 8, 10]
+        System.out.println(solution(4, 3)); // [4, 8, 12]
+        System.out.println(solution(-4, 2)); // [-4, -8]
+    }
+}
