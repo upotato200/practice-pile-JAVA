e41cead01e8e65b0a566ed4e2e80dff841bc0670
whlsls3377 <whlsls3377@naver.com>
2024-09-18 02:51:45 +0900

    내일배움캠프 자바 Spring 코드카타 사전캠프

--- src/SpartaCoding/test10/SpartaProblem14.java
+++ src/SpartaCoding/test10/SpartaProblem14.java
@@ -1,7 +1,22 @@
 package SpartaCoding.test10;
 
 public class SpartaProblem14 {
-    public static void main(String[] args) {
+    public static int solution(int n) {
+        int sum = 0;
 
+        // 1부터 n까지의 숫자 중 n의 약수를 찾아 합을 구함
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // 예시 테스트
+        System.out.println(solution(12)); // 28
+        System.out.println(solution(5));  // 6
     }
 }
--- src/SpartaCoding/test10/SpartaProblem15.java
+++ src/SpartaCoding/test10/SpartaProblem15.java
@@ -1,7 +1,19 @@
 package SpartaCoding.test10;
 
 public class SpartaProblem15 {
-    public static void main(String[] args) {
+    public static int solution(int n) {
+        // 1부터 n-1까지 탐색하며 n % x == 1을 만족하는 가장 작은 x를 찾음
+        for (int x = 2; x < n; x++) {
+            if (n % x == 1) {
+                return x;
+            }
+        }
+        return -1; // 문제의 조건 상 이 값은 실행되지 않음
+    }
 
+    public static void main(String[] args) {
+        // 예시 테스트
+        System.out.println(solution(10)); // 3
+        System.out.println(solution(12)); // 11
     }
 }
